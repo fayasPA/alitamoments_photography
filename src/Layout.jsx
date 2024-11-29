fe76ff7066a21f2b07c04b295f720ba3ed9b0bb3
fayasPA <fayasmuthalib@gmail.com>
2024-11-29 14:13:07 +0530

    footer

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Footer from './components/Footer.jsx';
+
 
 const Layout = () => {
 
